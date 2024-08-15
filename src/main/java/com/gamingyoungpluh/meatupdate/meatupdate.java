0782c3a652e5a7b7ae5e109c664b84205c7a36fd
GamingYoungPluh <gamingyoungpluh@gmail.com>
2024-08-15 15:24:15 -0700

    modified:   src/main/java/com/gamingyoungpluh/meatupdate/meatupdate.java

--- src/main/java/com/gamingyoungpluh/meatupdate/meatupdate.java
+++ src/main/java/com/gamingyoungpluh/meatupdate/meatupdate.java
@@ -1,11 +1,31 @@
 package com.gamingyoungpluh.meatupdate;
 
 
+import org.antlr.v4.parse.ANTLRParser.finallyClause_return;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;;
 
 @Mod("meatupdate")
 public class meatupdate {
+
+    public static final String MEAT_ID = "meatupdate";
+
+    public static final CreativeModeTab MEAT_TAB = new CreativeModeTab(MEAT_ID) {
+
+        @Override
+        @OnlyIn(Dist.CLIENT)
+        public ItemStack makeIcon() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+        
+    };
+
 
     public meatupdate() {
         MinecraftForge.EVENT_BUS.register(this);
